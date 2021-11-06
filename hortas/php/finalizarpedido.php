48e16918085b809dfdb44e05c6ca40606ade39ad
Cristian Krone <cristian_krone@hotmail.com>
2021-11-06 13:49:19 -0300

    Feito a função para o produtor finalizar o pedido e informar a data

--- hortas/php/finalizarpedido.php
+++ hortas/php/finalizarpedido.php
@@ -9,6 +9,11 @@ $finalizar_pedido = "UPDATE pedido SET data_entrega = NOW() WHERE cod_pedido = '
 $conecta_finalizar = mysqli_query($conn, $finalizar_pedido);
 
 
+$_SESSION['msg'] = "<p style='color: green;'>Pedido Entregue e Finalizado!!</p>";
+header("Location: gerarrelatorios.php?cod_produtor=$_SESSION[cod_produtor]");
+
+
+
 
 
 
